d361d0c82103e17b0efc4639cb4b0e47a4ed5270
Gabrielb0rn <gabriel_born@estudante.sesisenai.org.br>
2024-05-17 09:04:02 -0300

    Site da koenigsegg
    Site teste para treinar html, css e js.

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Este script será executado quando a página for carregada
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Página carregada e DOM completamente construído');
+    // Podemos adicionar mais funcionalidades aqui
+});
