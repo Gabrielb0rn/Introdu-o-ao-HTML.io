9e1dea275f2f427f19457264e230edae4b1b234b
Gabrielb0rn <gabriel_born@estudante.sesisenai.org.br>
2024-05-24 08:44:25 -0300

    Atualização

--- Projeto-aula2/resources/scripts.js
+++
@@ -1,8 +0,0 @@
-// Adiciona interatividade ou funcionalidades futuras se necessário
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Koenigsegg page is fully loaded');
-    // Exemplos de funcionalidades que podem ser adicionadas:
-    // - Carrossel de imagens
-    // - Formulário de contato
-    // - Animações
-});
