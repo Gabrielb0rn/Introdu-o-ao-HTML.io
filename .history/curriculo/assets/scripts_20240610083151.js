00cf4a4d6020689d8dd4b95bd75dd27324b3847e
Gabrielb0rn <gabriel_born@estudante.sesisenai.org.br>
2024-06-10 08:40:56 -0300

    Atualização

---
+++ .history/curriculo/assets/scripts_20240610083151.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const portfolioButton = document.getElementById('portfolioButton');
+    portfolioButton.addEventListener('click', () => {
+        window.location.href = 'https://seuportfolio.com'; // Substitua com o link do seu portfólio
+    });
+});
---
+++ .history/curriculo/assets/scripts_20240610083513.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const portfolioButton = document.querySelector('botao');
+    portfolioButton.addEventListener('click', () => {
+        window.location.href = 'https://gabrielborn5.wixsite.com/the-portf-lio---gabr';
+    });
+});
